5dc47156a600b9a2375b2e40375424eebcf3cdbc
Mike Dussault <mike-slang@sudolabs.com>
2012-07-06 06:33:00 -0700

    initial version
    - Supports changing font family, size, style, and character set.
    
    - Packs it all into a power-of-two texture that is as close to square as it can get.
    
    - This version doesn't have export, filters, or saving of configuration options.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace SudoFont
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if ( disposing && ( components != null ) )
+			{
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.Label _sizeLabel;
+			System.Windows.Forms.Label label1;
+			System.Windows.Forms.Label label2;
+			System.Windows.Forms.Label label3;
+			System.Windows.Forms.GroupBox groupBox1;
+			System.Windows.Forms.Label label4;
+			this._fontPreview = new System.Windows.Forms.Panel();
+			this._fontsList = new System.Windows.Forms.ListBox();
+			this._sizeCombo = new System.Windows.Forms.ComboBox();
+			this._outputPreview = new System.Windows.Forms.Panel();
+			this._outputImageSizeLabel = new System.Windows.Forms.Label();
+			this._boldOption = new System.Windows.Forms.CheckBox();
+			this._italicOption = new System.Windows.Forms.CheckBox();
+			this._underlineOption = new System.Windows.Forms.CheckBox();
+			this._strikeoutOption = new System.Windows.Forms.CheckBox();
+			this._characterSetControl = new System.Windows.Forms.TextBox();
+			_sizeLabel = new System.Windows.Forms.Label();
+			label1 = new System.Windows.Forms.Label();
+			label2 = new System.Windows.Forms.Label();
+			label3 = new System.Windows.Forms.Label();
+			groupBox1 = new System.Windows.Forms.GroupBox();
+			label4 = new System.Windows.Forms.Label();
+			groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// _fontPreview
+			// 
+			this._fontPreview.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this._fontPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this._fontPreview.Location = new System.Drawing.Point(12, 344);
+			this._fontPreview.Name = "_fontPreview";
+			this._fontPreview.Size = new System.Drawing.Size(469, 174);
+			this._fontPreview.TabIndex = 0;
+			this._fontPreview.Paint += new System.Windows.Forms.PaintEventHandler(this._fontPreview_Paint);
+			// 
+			// _fontsList
+			// 
+			this._fontsList.FormattingEnabled = true;
+			this._fontsList.Location = new System.Drawing.Point(12, 12);
+			this._fontsList.Name = "_fontsList";
+			this._fontsList.Size = new System.Drawing.Size(260, 264);
+			this._fontsList.TabIndex = 1;
+			this._fontsList.SelectedIndexChanged += new System.EventHandler(this._fontsList_SelectedIndexChanged);
+			// 
+			// _sizeLabel
+			// 
+			_sizeLabel.AutoSize = true;
+			_sizeLabel.Location = new System.Drawing.Point(10, 34);
+			_sizeLabel.Name = "_sizeLabel";
+			_sizeLabel.Size = new System.Drawing.Size(27, 13);
+			_sizeLabel.TabIndex = 2;
+			_sizeLabel.Text = "Size";
+			_sizeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// _sizeCombo
+			// 
+			this._sizeCombo.FormattingEnabled = true;
+			this._sizeCombo.Location = new System.Drawing.Point(43, 31);
+			this._sizeCombo.Name = "_sizeCombo";
+			this._sizeCombo.Size = new System.Drawing.Size(121, 21);
+			this._sizeCombo.TabIndex = 3;
+			this._sizeCombo.SelectedIndexChanged += new System.EventHandler(this._sizeCombo_SelectedIndexChanged);
+			this._sizeCombo.TextUpdate += new System.EventHandler(this._sizeCombo_TextUpdate);
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new System.Drawing.Point(12, 328);
+			label1.Name = "label1";
+			label1.Size = new System.Drawing.Size(69, 13);
+			label1.TabIndex = 4;
+			label1.Text = "Font Preview";
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new System.Drawing.Point(12, 542);
+			label2.Name = "label2";
+			label2.Size = new System.Drawing.Size(80, 13);
+			label2.TabIndex = 6;
+			label2.Text = "Output Preview";
+			// 
+			// _outputPreview
+			// 
+			this._outputPreview.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this._outputPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this._outputPreview.Location = new System.Drawing.Point(12, 558);
+			this._outputPreview.Name = "_outputPreview";
+			this._outputPreview.Size = new System.Drawing.Size(469, 174);
+			this._outputPreview.TabIndex = 5;
+			this._outputPreview.Paint += new System.Windows.Forms.PaintEventHandler(this._outputPreview_Paint);
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Location = new System.Drawing.Point(12, 735);
+			label3.Name = "label3";
+			label3.Size = new System.Drawing.Size(100, 13);
+			label3.TabIndex = 7;
+			label3.Text = "Output Image Size: ";
+			// 
+			// _outputImageSizeLabel
+			// 
+			this._outputImageSizeLabel.AutoSize = true;
+			this._outputImageSizeLabel.Location = new System.Drawing.Point(109, 735);
+			this._outputImageSizeLabel.Name = "_outputImageSizeLabel";
+			this._outputImageSizeLabel.Size = new System.Drawing.Size(0, 13);
+			this._outputImageSizeLabel.TabIndex = 8;
+			// 
+			// groupBox1
+			// 
+			groupBox1.Controls.Add(this._strikeoutOption);
+			groupBox1.Controls.Add(this._underlineOption);
+			groupBox1.Controls.Add(this._italicOption);
+			groupBox1.Controls.Add(this._boldOption);
+			groupBox1.Controls.Add(this._sizeCombo);
+			groupBox1.Controls.Add(_sizeLabel);
+			groupBox1.Location = new System.Drawing.Point(278, 12);
+			groupBox1.Name = "groupBox1";
+			groupBox1.Size = new System.Drawing.Size(177, 172);
+			groupBox1.TabIndex = 9;
+			groupBox1.TabStop = false;
+			groupBox1.Text = "Style";
+			// 
+			// _boldOption
+			// 
+			this._boldOption.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this._boldOption.Location = new System.Drawing.Point(47, 68);
+			this._boldOption.Name = "_boldOption";
+			this._boldOption.Size = new System.Drawing.Size(73, 20);
+			this._boldOption.TabIndex = 11;
+			this._boldOption.Text = "Bold";
+			this._boldOption.UseVisualStyleBackColor = true;
+			this._boldOption.CheckedChanged += new System.EventHandler(this._boldOption_CheckedChanged);
+			// 
+			// _italicOption
+			// 
+			this._italicOption.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this._italicOption.Location = new System.Drawing.Point(47, 91);
+			this._italicOption.Name = "_italicOption";
+			this._italicOption.Size = new System.Drawing.Size(73, 20);
+			this._italicOption.TabIndex = 12;
+			this._italicOption.Text = "Italic";
+			this._italicOption.UseVisualStyleBackColor = true;
+			this._italicOption.CheckedChanged += new System.EventHandler(this._italicOption_CheckedChanged);
+			// 
+			// _underlineOption
+			// 
+			this._underlineOption.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this._underlineOption.Location = new System.Drawing.Point(46, 114);
+			this._underlineOption.Name = "_underlineOption";
+			this._underlineOption.Size = new System.Drawing.Size(73, 20);
+			this._underlineOption.TabIndex = 13;
+			this._underlineOption.Text = "Underline";
+			this._underlineOption.UseVisualStyleBackColor = true;
+			this._underlineOption.CheckedChanged += new System.EventHandler(this._underlineOption_CheckedChanged);
+			// 
+			// _strikeoutOption
+			// 
+			this._strikeoutOption.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this._strikeoutOption.Location = new System.Drawing.Point(46, 137);
+			this._strikeoutOption.Name = "_strikeoutOption";
+			this._strikeoutOption.Size = new System.Drawing.Size(73, 20);
+			this._strikeoutOption.TabIndex = 14;
+			this._strikeoutOption.Text = "Strikeout";
+			this._strikeoutOption.UseVisualStyleBackColor = true;
+			this._strikeoutOption.CheckedChanged += new System.EventHandler(this._strikeoutOption_CheckedChanged);
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Location = new System.Drawing.Point(12, 299);
+			label4.Name = "label4";
+			label4.Size = new System.Drawing.Size(72, 13);
+			label4.TabIndex = 10;
+			label4.Text = "Character Set";
+			// 
+			// _characterSetControl
+			// 
+			this._characterSetControl.Location = new System.Drawing.Point(90, 296);
+			this._characterSetControl.Name = "_characterSetControl";
+			this._characterSetControl.Size = new System.Drawing.Size(391, 20);
+			this._characterSetControl.TabIndex = 11;
+			this._characterSetControl.TextChanged += new System.EventHandler(this._characterSetControl_TextChanged);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(726, 763);
+			this.Controls.Add(this._characterSetControl);
+			this.Controls.Add(label4);
+			this.Controls.Add(groupBox1);
+			this.Controls.Add(this._outputImageSizeLabel);
+			this.Controls.Add(label3);
+			this.Controls.Add(label2);
+			this.Controls.Add(this._outputPreview);
+			this.Controls.Add(label1);
+			this.Controls.Add(this._fontsList);
+			this.Controls.Add(this._fontPreview);
+			this.Name = "MainForm";
+			this.Text = "Sudo Font";
+			groupBox1.ResumeLayout(false);
+			groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel _fontPreview;
+		private System.Windows.Forms.ListBox _fontsList;
+		private System.Windows.Forms.ComboBox _sizeCombo;
+		private System.Windows.Forms.Panel _outputPreview;
+		private System.Windows.Forms.Label _outputImageSizeLabel;
+		private System.Windows.Forms.CheckBox _strikeoutOption;
+		private System.Windows.Forms.CheckBox _underlineOption;
+		private System.Windows.Forms.CheckBox _italicOption;
+		private System.Windows.Forms.CheckBox _boldOption;
+		private System.Windows.Forms.TextBox _characterSetControl;
+	}
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,525 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Drawing.Text;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace SudoFont
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			_fontPreview.BackColor = Color.Black;
+
+			// Setup a default character set.
+			ResetCharacterSet();
+
+			// Fill up the fonts listbox.
+			_allFonts = new InstalledFontCollection();
+			foreach ( FontFamily family in _allFonts.Families )
+			{
+				if ( family.IsStyleAvailable( FontStyle.Regular ) )
+					_fontsList.Items.Add( family.Name );
+			}
+
+			// Add sizes.
+			_sizeCombo.Items.Add( 6 );
+			_sizeCombo.Items.Add( 8 );
+			_sizeCombo.Items.Add( 9 );
+			_sizeCombo.Items.Add( 10 );
+			_sizeCombo.Items.Add( 11 );
+			_sizeCombo.Items.Add( 12 );
+			_sizeCombo.Items.Add( 14 );
+			_sizeCombo.Items.Add( 18 );
+			_sizeCombo.Items.Add( 24 );
+			_sizeCombo.Items.Add( 30 );
+			_sizeCombo.Items.Add( 36 );
+			_sizeCombo.Items.Add( 48 );
+			_sizeCombo.Items.Add( 60 );
+			_sizeCombo.Items.Add( 72 );
+
+			_fontStyleControls = new FontStyleControl[4];
+			_fontStyleControls[0] = new FontStyleControl() { Control = _boldOption, Style = FontStyle.Bold };
+			_fontStyleControls[1] = new FontStyleControl() { Control = _italicOption, Style = FontStyle.Italic };
+			_fontStyleControls[2] = new FontStyleControl() { Control = _underlineOption, Style = FontStyle.Underline };
+			_fontStyleControls[3] = new FontStyleControl() { Control = _strikeoutOption, Style = FontStyle.Strikeout };
+
+			_fontsList.SelectedItem = "Arial";
+			_sizeCombo.SelectedItem = 12;
+
+			Recalculate();
+		}
+
+		void Recalculate()
+		{
+			if ( _fontsList.SelectedItem == null || _sizeCombo.Text == "" )
+				return;
+
+			// Get the font family.
+			string fontFamily = _fontsList.SelectedItem.ToString();
+			FontFamily family = new FontFamily( fontFamily );
+
+			// Setup the FontStyle.
+			FontStyle style = 0;
+
+			if ( family.IsStyleAvailable( FontStyle.Regular ) )
+				style = FontStyle.Regular;
+
+			foreach ( var ctl in _fontStyleControls )
+			{
+				if ( ctl.Control.Checked && family.IsStyleAvailable( style & ctl.Style ) )
+					style |= ctl.Style;
+			}
+
+			// Figure out the size.
+			int size = 12;
+			try
+			{
+				size = Convert.ToInt32( _sizeCombo.Text );
+			}
+			catch ( Exception )
+			{
+			}
+
+			_currentFont = new Font( fontFamily, size, style );
+
+			BuildPackedImage();
+
+			if ( _packedImage != null )
+				_outputImageSizeLabel.Text = string.Format( "{0} x {1}", _packedImage.Width, _packedImage.Height );
+			else
+				_outputImageSizeLabel.Text = "( error )";
+
+			_fontPreview.Invalidate();
+		}
+
+		[DebuggerDisplay( "Char: {Character}, {Width} x {Height}, Packed: ({PackedX}, {PackedY})" )]
+		class CharacterInfo
+		{
+			public Char Character;
+			
+			// If you print the character into a Graphics at (0,0), this will be where the actual nonzero data is.
+			public int XOffset;
+			public int YOffset;
+			public int Width;
+			public int Height;
+
+			// Where this character is in the packed texture.
+			public int PackedX;
+			public int PackedY;
+			
+			// This is the original image data after Graphics.DrawString.
+			// Dimensions are Width x Height.
+			public UInt32[] Image;
+		}
+
+		void BuildPackedImage()
+		{
+			_packedImage = null;
+			_outputPreview.Invalidate();
+
+			// Remove any duplicates from the character set.
+			string characterSet = new string( _characterSetControl.Text.Union( _characterSetControl.Text ).ToArray() );
+
+			CharacterInfo[] infos = new CharacterInfo[ characterSet.Length ];
+
+			Brush whiteBrush = new SolidBrush( Color.White );
+			Brush blackBrush = new SolidBrush( Color.Black );
+
+			// Get all the bitmap data and extents for the characters.
+			using ( Bitmap tempBitmap = new Bitmap( 512, 512, PixelFormat.Format32bppArgb ) )
+			{
+				using ( Graphics g = Graphics.FromImage( tempBitmap ) )
+				{
+					// Initialize it to black to start.
+					g.Clear( Color.Black );
+
+					// Initialize each CharacterInfo.
+					for ( int i=0; i < characterSet.Length; i++ )
+					{
+						CharacterInfo c = new CharacterInfo();
+						infos[i] = c;
+
+						c.Character = characterSet[i];
+
+						// Draw this character into tempBitmap.
+						g.DrawString( c.Character.ToString(), _currentFont, whiteBrush, new Point( 0, 0 ) );
+
+						// Scan to find the extents.
+						Rectangle? startingExtentsNullable = GetMaximumCharacterExtents( g, _currentFont, c.Character );
+						if ( !startingExtentsNullable.HasValue )
+							return;
+
+						// Make sure we're not accessing outside the bitmap itself. If this letter is larger than 512x512,
+						// then we've got bigger problems elsewhere!
+						Rectangle startingExtents = startingExtentsNullable.Value;
+						startingExtents.Width = Math.Min( startingExtents.Width, tempBitmap.Width );
+						startingExtents.Height = Math.Min( startingExtents.Height, tempBitmap.Height );
+
+						Rectangle extents;
+						ExtractImageData( tempBitmap, startingExtents, out c.Image, out extents );
+						c.XOffset = extents.X;
+						c.YOffset = extents.Y;
+						c.Width = extents.Width;
+						c.Height = extents.Height;
+
+						g.FillRectangle( blackBrush, new Rectangle( 0, 0, extents.X + extents.Width + 1, extents.Y + extents.Height + 1 ) );
+					}
+				}
+			}
+
+			// Figure out the max character size.
+			Size maxSize = new System.Drawing.Size( 0, 0 );
+			foreach ( var c in infos )
+			{
+				maxSize.Width  = Math.Max( maxSize.Width, c.Width );
+				maxSize.Height = Math.Max( maxSize.Height, c.Height );
+			}
+
+			int maxImageDimension = 1024;
+
+			// Now, pack all the characters in the output.
+			// First, sort by height.
+			CharacterInfo[] sorted = infos.OrderBy( x => -x.Height ).ToArray();
+			int packedWidth = 16;
+			int packedHeight = 0;
+			while ( packedWidth < maxImageDimension )
+			{
+				// We started out with a tiny width, so the height was super likely to be larger than the width.
+				// If we finally get it where the width and height are equal, or where width is just one power of 
+				// two larger, then we're happy.
+				packedHeight = PackCharacters( sorted, packedWidth );
+				if ( NextPowerOfTwo( packedWidth ) >= packedHeight )
+					break;
+
+				packedWidth <<= 1;
+			}
+
+			// Now render the final bitmap.
+			_packedImage = new Bitmap( packedWidth, NextPowerOfTwo( packedHeight ) );
+			RenderPackedImage( _packedImage, infos );
+		}
+
+		static void RenderPackedImage( Bitmap bitmap, CharacterInfo[] infos )
+		{
+			using ( Graphics g = Graphics.FromImage( bitmap ) )
+			{
+				g.Clear( Color.Black );
+			}
+
+			BitmapData bm = bitmap.LockBits( new Rectangle( 0, 0, bitmap.Width, bitmap.Height ), ImageLockMode.WriteOnly, bitmap.PixelFormat );
+			
+			foreach ( CharacterInfo c in infos )
+				CopyImageData( c.Image, c.Width, c.Height, bm, c.PackedX, c.PackedY );
+
+			bitmap.UnlockBits( bm );
+		}
+
+		static void CopyImageData( uint[] imageData, int imageWidth, int imageHeight, BitmapData dest, int destX, int destY )
+		{
+			unsafe
+			{
+				uint *pDestBase = (uint*)dest.Scan0;
+
+				for ( int y=0; y < imageHeight; y++ )
+				{
+					int outY = destY + y;
+					if ( outY < 0 || outY > dest.Height )
+						continue;
+
+					for ( int x=0; x < imageWidth; x++ )
+					{
+						int outX = destX + x;
+						if ( outX < 0 || outX > dest.Width )
+							continue;
+
+						pDestBase[ outY * ( dest.Stride >> 2 ) + outX ] = imageData[ y * imageWidth + x ];
+					}
+				}
+			}
+		}
+
+		// We're assuming that the infos are sorted by height.
+		// Returns the max Y value of any character.
+		static int PackCharacters( CharacterInfo[] infos, int width, int extraPadX = 0, int extraPadY = 0 )
+		{
+			// Find runs..
+			int firstChar = 0;
+			int curY = 0;
+			for ( int iRun=0; iRun < int.MaxValue; iRun++ )
+			{
+				if ( firstChar == infos.Length )
+					break;
+
+				// Figure out how many characters we can fit horizontally on this line.
+				int lineWidth = infos[ firstChar ].Width;
+				int lastChar;
+				for ( lastChar=firstChar+1; lastChar < infos.Length; lastChar++ )
+				{
+					if ( lastChar != firstChar )
+					{
+						if ( ( lineWidth + extraPadX + infos[lastChar].Width ) > width )
+							break;
+
+						lineWidth += extraPadX;
+					}
+					
+					lineWidth += infos[lastChar].Width;
+				}
+
+				int numCharsInLine = lastChar - firstChar;
+
+				// Figure out the height of the line.
+				int maxHeight = 0;
+				for ( int i=0; i < numCharsInLine; i++ )
+					maxHeight = Math.Max( maxHeight, infos[ firstChar + i ].Height );
+
+				// Now just add each character in there.
+				int curX = 0;
+				for ( int i=0; i < numCharsInLine; i++ )
+				{
+					infos[ firstChar + i ].PackedX = curX;
+					infos[ firstChar + i ].PackedY = curY;
+					curX += infos[ firstChar + i ].Width + extraPadX;
+				}
+
+				firstChar += numCharsInLine;
+				curY += maxHeight + extraPadY;
+			}
+			
+			return curY;
+		}
+
+		static int NextPowerOfTwo( int i )
+		{
+			int ret = 1;
+			
+			while ( ret < i )
+				ret <<= 1;
+
+			return ret;
+		}
+
+		static void ExtractImageData( Bitmap bitmap, Rectangle startingExtents, out UInt32[] data, out Rectangle extents )
+		{
+			BitmapData bm = bitmap.LockBits( new Rectangle( 0, 0, bitmap.Width, bitmap.Height ), ImageLockMode.ReadOnly, bitmap.PixelFormat );
+
+			// Scan for Y extents.
+			int minY = int.MaxValue;
+			int maxY = int.MinValue;
+			for ( int y = startingExtents.Y; y < startingExtents.Y + startingExtents.Height; y++ )
+			{
+				if ( !IsRowEmpty( bm, y, startingExtents.X, startingExtents.Width ) )
+				{
+					minY = Math.Min( y, minY );
+					maxY = Math.Max( y, maxY );
+				}
+			}
+			
+			// Scan for X extents.
+			int minX = int.MaxValue;
+			int maxX = int.MinValue;
+			for ( int x = startingExtents.X; x < startingExtents.X + startingExtents.Width; x++ )
+			{
+				if ( !IsColumnEmpty( bm, x, startingExtents.Y, startingExtents.Height ) )
+				{
+					minX = Math.Min( x, minX );
+					maxX = Math.Max( x, maxX );
+				}
+			}
+
+			// Setup the extents rect.
+			if ( minX == int.MaxValue || minY == int.MaxValue || maxX == int.MinValue || maxY == int.MinValue )
+				extents = new Rectangle( startingExtents.X, startingExtents.Y, 0, 0 );
+			else
+				extents = new Rectangle( minX, minY, maxX - minX + 1, maxY - minY + 1 );
+
+			// Copy the data to an output array.
+			data = new uint[ extents.Width * extents.Height ];
+
+			unsafe
+			{
+				uint *pSrc = (uint*)bm.Scan0;
+				int uintStride = bm.Stride >> 2;
+
+				for ( int y=0; y < extents.Height; y++ )
+				{
+					for ( int x=0; x < extents.Width; x++ )
+					{
+						data[ y * extents.Width + x ] = pSrc[ ( y + extents.Y ) * uintStride + ( x + extents.X ) ];
+					}
+				}
+			}
+
+			bitmap.UnlockBits( bm );
+		}
+
+		static bool IsRowEmpty( BitmapData bm, int row, int startX, int width )
+		{
+			unsafe
+			{
+				UInt32 *pCur = (UInt32*)bm.Scan0 + ( row * (bm.Stride>>2) ) + startX;
+				UInt32 *pEnd = pCur + width;
+				while ( pCur < pEnd )
+				{
+					if ( *pCur != 0xFF000000 )
+						return false;
+
+					++pCur;
+				}
+			}
+
+			return true;
+		}
+
+		static bool IsColumnEmpty( BitmapData bm, int column, int startY, int height )
+		{
+			int strideInPixels = bm.Stride >> 2;
+
+			unsafe
+			{
+				UInt32 *pCur = (UInt32*)bm.Scan0 + column;
+				UInt32 *pEnd = pCur + height * strideInPixels;
+				while ( pCur < pEnd )
+				{
+					if ( *pCur != 0xFF000000 )
+						return false;
+
+					pCur += strideInPixels;
+				}
+			}
+
+			return true;
+		}
+
+		static Rectangle? GetMaximumCharacterExtents( Graphics g, Font font, Char ch )
+		{
+			SizeF size = g.MeasureString( ch.ToString(), font );
+			return new Rectangle( 0, 0, (int)Math.Ceiling( size.Width ), (int)Math.Ceiling( size.Height ) );
+
+
+			StringFormat stringFormat = new StringFormat( StringFormat.GenericTypographic );
+			stringFormat.SetMeasurableCharacterRanges( new [] { new CharacterRange( 0, 1 ) } );
+			Region[] regions = g.MeasureCharacterRanges( ch.ToString(), font, Rectangle.Empty, stringFormat );
+						
+			if ( regions.Length == 0 )
+				return null;
+
+			RectangleF bounds = regions[0].GetBounds( g );
+			foreach ( var region in regions.Take( 1 ) )
+				bounds = RectangleF.Union( bounds, region.GetBounds( g ) );
+
+			return new Rectangle( (int)bounds.X, (int)bounds.Y, (int)Math.Ceiling( bounds.Width ), (int)Math.Ceiling( bounds.Height ) );
+		}
+
+		private void _fontPreview_Paint( object sender, PaintEventArgs e )
+		{
+			if ( _currentFont == null )
+				return;
+
+			Graphics g = e.Graphics;
+
+			int curY = 0;
+			foreach ( string line in _previewText.Split( new [] { '\n' } ) )
+			{
+				g.DrawString( line, _currentFont, new SolidBrush( Color.White ), new Point( 0, curY ) );
+				curY += _currentFont.Height;
+			}
+		}
+
+		private void _outputPreview_Paint( object sender, PaintEventArgs e )
+		{
+			if ( _packedImage == null )
+				return;
+
+			Graphics g = e.Graphics;
+			g.DrawImage( _packedImage, new Point( 0, 0 ) );
+		}
+
+		private void _fontsList_SelectedIndexChanged( object sender, EventArgs e )
+		{
+			// Get the font family.
+			string fontFamily = _fontsList.SelectedItem.ToString();
+			FontFamily family = new FontFamily( fontFamily );
+
+			// Figure out which styles are available.
+			foreach ( var ctl in _fontStyleControls )
+			{
+				ctl.Control.Enabled = family.IsStyleAvailable( ctl.Style );
+				ctl.Control.Checked &= ctl.Control.Enabled;	// Restrict the current settings to what's available.
+			}
+			
+			Recalculate();
+		}
+
+		private void _boldOption_CheckedChanged( object sender, EventArgs e )
+		{
+			Recalculate();
+		}
+
+		private void _italicOption_CheckedChanged( object sender, EventArgs e )
+		{
+			Recalculate();
+		}
+
+		private void _underlineOption_CheckedChanged( object sender, EventArgs e )
+		{
+			Recalculate();
+		}
+
+		private void _strikeoutOption_CheckedChanged( object sender, EventArgs e )
+		{
+			Recalculate();
+		}
+
+		private void _sizeCombo_TextUpdate( object sender, EventArgs e )
+		{
+			Recalculate();
+		}
+
+		private void _sizeCombo_SelectedIndexChanged( object sender, EventArgs e )
+		{
+			Recalculate();
+		}
+
+		private void _characterSetControl_TextChanged( object sender, EventArgs e )
+		{
+			Recalculate();
+		}
+
+		void ResetCharacterSet()
+		{
+			string characterSetText = "";
+			for ( int i=32; i <= 127; i++ )
+			{
+				characterSetText += ( (Char)i ).ToString();
+			}
+			_characterSetControl.Text = characterSetText;
+		}
+
+
+
+		string _previewText = "0123456789 _*+- ()[]#@\nABCDEFGHIJKLMNOPQRSTUVWXYZ\nabcdefghijklmnopqrstuvwxyz";
+
+		struct FontStyleControl
+		{
+			public CheckBox Control;
+			public FontStyle Style;
+		}
+
+		FontStyleControl[] _fontStyleControls;
+		Bitmap _packedImage;
+
+		Font _currentFont;
+		InstalledFontCollection _allFonts;
+	}
+}
